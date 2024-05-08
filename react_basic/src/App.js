a162563f4e1191d372c6619cf15439c6796a3d7c
masato-210831 <b86500b@gmail.com>
2024-05-08 22:02:32 +0900

    react_django Udemy: Reducer, Callback

--- react_basic/src/App.js
+++ react_basic/src/App.js
@@ -3,19 +3,71 @@ import './App.css';
 // import BasicUseEffect from './components/BasicUseEffect';
 // import TimerContainer from './components/TimerContainer';
 import ApiFetch from './components/ApiFetch';
+import AppContext from './contexts/AppContext';
+import B from './components/B';
+import BasicReducer from './components/BasicReducer';
+import CompB from './components/CompB';
+import  {useReducer, useState, useCallback} from 'react'
+import Memo from './components/Memo';
+import CountDisplay from './components/CountDisplay';
+import CountClick from './components/CountClick';
+
+
+
+
+const initialState = 0
+const reducer = (currentState, action) => {
+  switch (action) {
+    case 'add_1':
+      return currentState + 1
+    case 'multiple_3':
+      return currentState * 3
+    case 'reset':
+      return initialState
+    default:
+      return currentState
+  }
+}
+
+
+
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState)
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const AddCount1 = useCallback(() => {
+    setCount1(prev => prev + 1)
+  }, [count1]);
+
+  const AddCount2 = useCallback(() => {
+    setCount2(prev => prev + 2)
+  }, [count2]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <Basic2/> */}
-        {/* <BasicUseEffect/> */}
-        {/* <TimerContainer/> */}
-        <ApiFetch />
-        
-      </header>
-    </div>
+    <AppContext.Provider value={{countProvided: count, dispatchProvided:dispatch}}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/* <Basic2/> */}
+          {/* <BasicUseEffect/> */}
+          {/* <TimerContainer/> */}
+          {/* <ApiFetch /> */}
+          {/* <BasicReducer /> */}
+          {/* Count {count}
+          <CompB /> */}
+          {/* < Memo /> */}
+          <CountDisplay name='count1' count={count1} />
+          <CountClick handleClick={AddCount1} >AddCount1</CountClick>
+          <CountDisplay name='count2' count={count2} />
+          <CountClick handleClick={AddCount2} >AddCount2</CountClick>
+
+        </header>
+      </div>
+    </AppContext.Provider>
   );
 }
 
---
+++ react_basic/src/components/Memo.js
@@ -0,0 +1,43 @@
+import React, {useState, useMemo} from 'react'
+
+const Memo = () => {
+
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const AddCount1 = () => {
+    setCount1(prev => prev + 1)
+  }
+
+  const AddCount2 = () => {
+    setCount2(prev => prev + 2)
+  };
+
+  // const isOdd = () => {
+  //   let i = 0
+  //   while(i < 500000000){
+  //     i++
+  //   }
+  //   return count1 % 2 !== 0
+  // }
+  const isOdd = useMemo(() => {
+    let i = 0
+    while(i < 500000000){
+      i++
+    }
+    return count1 % 2 !== 0
+  }, [count1])
+
+
+
+  return (
+    <div>
+      <button onClick={AddCount1} >Count1 : {count1}</button>
+      <button onClick={AddCount2} >Count2 : {count2}</button>
+      <br />
+      <span>{isOdd ? 'Odd number':'Even numver'}</span>
+    </div>
+  )
+}
+
+export default Memo
